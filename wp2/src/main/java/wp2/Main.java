92973326bb985305d1583ff4614fa8c72a5e9de3
Shun Tang <14686039@qq.com>
2015-07-08 09:33:37 +0800

    QQ

---
+++ wp2/src/main/java/wp2/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2014 Nanjing Sesan Medical Technology Co.,Ltd.
+ * All Rights Reserved
+ * The software and information contained herein are proprietary to, and
+ * comprise valuable trade secrets of, Nanjing Sesan Medical Technology Co.,Ltd., 
+ * which intends to preserve as trade secrets such software and information.
+ * This software is an unpublished copyright of Nanjing Sesan Medical Technology Co.,Ltd.. 
+ * and may not be used, copied, transmitted, or stored in any manner. 
+ * This software and information or any other copies thereof may
+ * not be provided or otherwise made available to any other person.
+ */
+
+package wp2;
+
+/**
+ * @author tangshun
+ * 2015年7月8日
+ * Ver 1.0
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
